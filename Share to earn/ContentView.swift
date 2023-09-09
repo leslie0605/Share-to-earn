82cf3dff8b03a960af676b9afa4493bf2afb0bca
leslie0605 <737888086@qq.com>
2023-09-09 11:14:15 +0800

    Initial Commit

---
+++ Share to earn/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  Share to earn
+//
+//  Created by 赵玉凡 on 2023/9/9.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Share to earn/Share_to_earnApp.swift
@@ -0,0 +1,17 @@
+//
+//  Share_to_earnApp.swift
+//  Share to earn
+//
+//  Created by 赵玉凡 on 2023/9/9.
+//
+
+import SwiftUI
+
+@main
+struct Share_to_earnApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
